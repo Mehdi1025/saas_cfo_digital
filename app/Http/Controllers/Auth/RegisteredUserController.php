dc193a3c4ae16030bc290327fc8593b310f2ab8d
Mahdi-bachiri <bachirimahdi006@gmail.com>
2026-07-20 01:17:01 +0200

    Autorise le dossier public/build pour le déploiement Cloudways

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -51,6 +51,6 @@ class RegisteredUserController extends Controller
             return redirect()->route('billing.checkout.start');
         }
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('verification.notice', absolute: false));
     }
 }
